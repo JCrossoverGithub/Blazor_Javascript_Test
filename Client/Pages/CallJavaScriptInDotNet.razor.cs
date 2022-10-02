26c7360307700db769b05e5e92f37cc0f06274e4
Josh <schultzjosh0@gmail.com>
2022-10-01 21:41:01 -0400

    Can call JS code using this guide: https://code-maze.com/how-to-call-javascript-code-from-net-blazor-webassembly/

---
+++ Client/Pages/CallJavaScriptInDotNet.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Javascript_Test.Client.Pages
+{
+    public partial class CallJavaScriptInDotNet
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        private IJSObjectReference _jsModule;
+        private string _registrationResult;
+        protected override async Task OnInitializedAsync()
+        {
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/jsExamples.js");
+        }
+
+        public async Task ShowAlertWindow()
+        {
+            await _jsModule.InvokeVoidAsync("showAlert", "JS function called from .NET");
+        }
+
+        private async Task RegisterEmail() =>
+            _registrationResult = await _jsModule.InvokeAsync<string>("emailRegistration", "Please provide your email");
+    }
+}
---
+++ Client/wwwroot/scripts/jsExamples.js
@@ -0,0 +1,11 @@
+﻿export function showAlert(message) {
+    alert(message);
+}
+
+export function emailRegistration(message) {
+    const result = prompt(message);
+    if (result === '' || result === null)
+        return 'Please provide an email'
+    const returnMessage = 'Hi ' + result.split('@')[0] + ' your email: ' + result + ' has been accepted.';
+    return returnMessage;
+}
